0af6a4c48989c47f374bcb85c67aaf7223d4364e
yi-yun <yi--yun@outlook.com>
2019-02-18 22:30:19 +0800

    mod everyday record

---
+++ java_se/src/main/java/com/yiyun/concurrent/T12.java
@@ -0,0 +1,33 @@
+package com.yiyun.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+public class T12 {
+    volatile boolean running = true; //对比一下有无volatile的情况下，整个程序运行结果的区别
+    void m() {
+        System.out.println("m start");
+        while(running) {
+			/*
+			try {
+				TimeUnit.MILLISECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}*/
+        }
+        System.out.println("m end!");
+    }
+    public static void main(String[] args) {
+        T12 t12 = new T12();
+
+        new Thread(t12::m, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t12.running = false;
+
+    }
+}
