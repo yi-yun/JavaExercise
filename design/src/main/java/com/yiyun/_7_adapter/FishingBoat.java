8324c73e1f3e3beb0dd40240f475a1a036b2847b
yi-yun <yi--yun@outlook.com>
2019-03-02 13:04:08 +0800

    add logback slf4j

--- design/src/main/java/com/yiyun/_7_adapter/FishingBoat.java
+++ design/src/main/java/com/yiyun/_7_adapter/FishingBoat.java
@@ -1,7 +1,12 @@
 package com.yiyun._7_adapter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FishingBoat {
+    private static final Logger LOGGER=LoggerFactory.getLogger(FishingBoat.class);
     public void sail() {
+        LOGGER.info("test log");
         System.out.println("The fishing boat is sailing");
     }
 }
