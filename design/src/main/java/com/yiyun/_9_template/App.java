ec2d9d69298fc4d955c9ee00fc6183edd3cc1aa4
yi-yun <yi--yun@outlook.com>
2019-03-07 18:25:01 +0800

    add 3.7 record

---
+++ design/src/main/java/com/yiyun/_9_template/App.java
@@ -0,0 +1,9 @@
+package com.yiyun._9_template;
+
+public class App {
+    public static void main(String[] args) {
+        WoodenHouse woodenHouse = new WoodenHouse();
+        woodenHouse.buildPillars();
+        woodenHouse.buildWalls();
+    }
+}
