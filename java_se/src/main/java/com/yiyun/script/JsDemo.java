e63ad97f9810dd378b393db95ded16bace2cf719
yi-yun <yi--yun@outlook.com>
2019-03-16 14:41:14 +0800

    add js resource

--- java_se/src/main/java/com/yiyun/script/JsDemo.java
+++ java_se/src/main/java/com/yiyun/script/JsDemo.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import java.io.FileReader;
+import java.net.URL;
 
 public class JsDemo {
     @Test
@@ -11,11 +13,16 @@ public class JsDemo {
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("javascript");
         engine.put("msg","yi-yun is headsome");
-        String str="var user={name:'yi-yun',age:18,schools:['qwerer','micahel']};";
+        String str="var user={name:'yi-yun',age:18,schools:['qwer','michael']};";
         str+="print(user.name)";
         //------------------------
         engine.eval(str);
         engine.eval("msg='yi-yun';");
         System.out.println(engine.get("msg"));
+
+        URL resource = JsDemo.class.getClassLoader().getResource("yiyun.js");
+        FileReader fileReader = new FileReader(resource.getPath());
+        engine.eval(fileReader);
+        fileReader.close();
     }
 }
---
+++ java_se/src/main/resources/yiyun.js
@@ -0,0 +1,6 @@
+function add() {
+    var a=3;
+    var b=4;
+    print(a+b);
+}
+add()
