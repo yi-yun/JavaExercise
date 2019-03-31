9e3ecc198d7db32a63175a52cbf5977743b76f5d
yi-yun <yi--yun@outlook.com>
2019-03-31 15:21:37 +0800

    mov spring scope

---
+++ spring/src/com/yiyun/scope/App.java
@@ -0,0 +1,29 @@
+package com.yiyun.scope;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+public class App {
+
+
+    @Autowired
+    private Dog dog1;
+    @Autowired
+    private Dog dog2;
+
+    @Autowired
+    private Dog dog3;
+    @Test
+    public void fun(){
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+        System.out.println(dog3);
+    }
+
+
+}
---
+++ spring/src/com/yiyun/scope/Dog.java
@@ -0,0 +1,7 @@
+package com.yiyun.scope;
+
+public class Dog {
+    public Dog() {
+        System.out.println("construct Dog");
+    }
+}
