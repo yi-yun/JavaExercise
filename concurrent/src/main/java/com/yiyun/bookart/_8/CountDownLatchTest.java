6699966940f2c8ca6ac2dfad449c24d76b4d74e2
yi-yun <yi--yun@outlook.com>
2019-04-29 11:29:35 +0800

    add countDownlatch

---
+++ concurrent/src/main/java/com/yiyun/bookart/_8/CountDownLatchTest.java
@@ -0,0 +1,18 @@
+package com.yiyun.bookart._8;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+    static CountDownLatch c=new CountDownLatch(2);
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(()->{
+            System.out.println(1);
+            c.countDown();
+            System.out.println(2);
+            c.countDown();
+        }).start();
+        c.await();
+        System.out.println(3);
+    }
+}
