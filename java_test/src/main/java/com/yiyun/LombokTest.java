5bb9ba8332029f1f9ff1bab0eb5b7da7a5379187
yi-yun <yi--yun@outlook.com>
2019-05-02 17:20:25 +0800

    add lombok slf4j

---
+++ java_test/src/main/java/com/yiyun/LombokTest.java
@@ -0,0 +1,10 @@
+package com.yiyun;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LombokTest {
+    public static void main(String[] args) {
+        log.info("yi-yun Test...");
+    }
+}
