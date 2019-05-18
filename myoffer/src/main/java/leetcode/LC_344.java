2648ae7f14c7ed5bfc9b5390e959bf42c47ff9a5
yi-yun <yi--yun@outlook.com>
2019-05-18 22:25:53 +0800

    add lc344

---
+++ myoffer/src/main/java/leetcode/LC_344.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class LC_344 {
+    public void swap(char[] s, int i, int j) {
+        char tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+
+    public void reverseString(char[] s) {
+        int slow = 0;
+        int fast = s.length - 1;
+        while (slow <= fast) {
+
+            swap(s, slow, fast);
+            slow++;
+            fast--;
+
+        }
+    }
+
+    @Test
+    public void fun() {
+        char[] chars = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(chars);
+        System.out.println("chars = " + chars);
+    }
+}
