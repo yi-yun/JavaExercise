bc2a7a3d0cbf0a1e65c399aac740f51e9fbd5143
yi-yun <yi--yun@outlook.com>
2019-05-19 20:09:30 +0800

    jvm init

---
+++ java_se/src/main/java/com/yiyun/jvm/_7/ConstClass.java
@@ -0,0 +1,8 @@
+package com.yiyun.jvm._7;
+
+public class ConstClass {
+    static {
+        System.out.println("ConstClass init......");
+    }
+    public static final String HELLOWORLD ="hello world";
+}
--- java_se/src/main/java/com/yiyun/jvm/_7/NotInit.java
+++ java_se/src/main/java/com/yiyun/jvm/_7/NotInit.java
@@ -2,7 +2,8 @@ package com.yiyun.jvm._7;
 
 public class NotInit {
     public static void main(String[] args) {
-//        System.out.println(SubClass.value);
+        System.out.println(SubClass.value);
         SuperClass[] sca=new SuperClass[10];
+        System.out.println("ConstClass.HELLOWORLD = " + ConstClass.HELLOWORLD);
     }
 }
