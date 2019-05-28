fb27a4502cd0bcbda1b2fd517e982380aae3f643
yi-yun <yi--yun@outlook.com>
2019-05-28 20:40:47 +0800

    add java_8_3

--- java_se/src/main/java/com/yiyun/java8/chap3/_3_3ExecuteAround.java
+++ java_se/src/main/java/com/yiyun/java8/chap3/_3_3ExecuteAround.java
@@ -1,7 +1,12 @@
 package com.yiyun.java8.chap3;
 
+import org.junit.Test;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class _3_3ExecuteAround {
     public static String processFile() throws Exception{
@@ -21,9 +26,25 @@ public class _3_3ExecuteAround {
         }
     }
 
+    public  static <T> void forEach(List<T> list, Consumer<T> c){
+        for (T i:list) {
+            c.accept(i);
+        }
+    }
+    
+    @Test
+    public void fun(){
+        forEach(Arrays.asList(1,2,53,4,3),
+            (Integer i)-> System.out.println("i = " + i));
+    }
     public static void main(String[] args) throws Exception{
-        processFile((BufferedReader br)->br.readLine());
-        processFile((BufferedReader br)->br.readLine()+br.readLine());
+//        processFile((BufferedReader br)->br.readLine());
+//        processFile((BufferedReader br)->br.readLine()+br.readLine());
+        forEach(Arrays.asList(1,2,3,4,5),
+            (Integer i)-> {
+            if (i>=3) System.out.println("i = " + i);
+        });
+        
     }
   
 }
