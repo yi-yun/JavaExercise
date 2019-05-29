bbb2449b8e88c4e99b0a0e6859605db1780d9173
yi-yun <yi--yun@outlook.com>
2019-05-29 20:23:20 +0800

    mod model

--- java_se/src/main/java/com/yiyun/java8/chap4/Dish.java
+++ java_se/src/main/java/com/yiyun/java8/chap4/Dish.java
@@ -1,8 +1,9 @@
 package com.yiyun.java8.chap4;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 public class Dish {
@@ -17,4 +18,19 @@ public class Dish {
         this.calories = calories;
         this.type = type;
     }
+
+    
+    public enum Type {MEAT, FISH, OTHER}
+
+
+    public static final List<Dish> menu =
+        Arrays.asList(new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH));
 }
