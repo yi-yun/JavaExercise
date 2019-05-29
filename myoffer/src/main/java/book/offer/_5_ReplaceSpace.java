42d5d35f460576cd29f72a1eef38675cbfddbfb6
yi-yun <yi--yun@outlook.com>
2019-05-29 21:54:30 +0800

    add offer 5&6

---
+++ myoffer/src/main/java/book/offer/_5_ReplaceSpace.java
@@ -0,0 +1,24 @@
+package book.offer;
+
+import org.junit.Test;
+
+public class _5_ReplaceSpace {
+    public String replace(String str){
+        if (str==null)
+            return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (String.valueOf(str.charAt(i)).equals(" ")){
+                stringBuilder.append("%20");
+            }else {
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+    @Test
+    public void fun(){
+        String we_are_happy = replace("We are happy");
+        
+    }
+}
