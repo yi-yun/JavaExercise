2902a30fec959c1455fc2560c28c6870b7763ddf
yi-yun <yi--yun@outlook.com>
2019-06-01 20:09:47 +0800

    add stackToQueue

---
+++ myoffer/src/main/java/book/offer/_9_2StackToQueue.java
@@ -0,0 +1,61 @@
+package book.offer;
+
+import java.util.Stack;
+
+public class _9_2StackToQueue {
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.push(5);
+        int param_2 = obj.peek();
+        int param_3 = obj.pop();
+        boolean param_4 = obj.empty();
+    }
+}
+class MyQueue {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1=new Stack<>();
+        stack2=new Stack<>();;
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (!empty()){
+            if (stack2.isEmpty()){
+                while (!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (!empty()){
+            if (stack2.isEmpty()){
+                while (!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty()&&stack2.isEmpty();
+    }
+}
