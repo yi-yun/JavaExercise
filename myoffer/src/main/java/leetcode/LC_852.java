8251e75118ff532f3ce0ca1d9bce8100fb3e7b4a
yi-yun <yi--yun@outlook.com>
2019-06-03 20:48:59 +0800

    add LC_852

---
+++ myoffer/src/main/java/leetcode/LC_852.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class LC_852 {
+    public int peakIndexInMountainArray(int[] A) {
+        int length = A.length;
+        int left=0;
+        int right=length-1;
+        while (left<=right){
+            int mid = ((right - left) >> 1) + left;
+            if(A[mid - 1] < A[mid] && A[mid] < A[mid + 1])
+                left = mid + 1;
+            else if(A[mid - 1] > A[mid] && A[mid] > A[mid + 1])
+                right = mid - 1;
+            else if(A[mid - 1] < A[mid] && A[mid] > A[mid + 1])
+                return mid;
+        }
+        return -1;
+    }
+    @Test
+    public void fun(){
+        int[] arrays = {0, 1, 2, 3, 1, 0};
+        int index = peakIndexInMountainArray(arrays);
+        System.out.println("index = " + index);
+    }
+}
