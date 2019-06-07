fbcd25837dc2bec6d6a5c86b814300e84a522c6e
yi-yun <yi--yun@outlook.com>
2019-06-07 14:27:35 +0800

    add == test

--- java_test/src/main/java/com/yiyun/JustTest.java
+++ java_test/src/main/java/com/yiyun/JustTest.java
@@ -10,29 +10,37 @@ public class JustTest {
     public JustTest() {
         System.out.println("b.....");
     }
+
     static {
         System.out.println("c.....");
     }
 
     public static void main(String[] args) {
         JustTest justTest = new JustTest();
-        System.out.println("-----------------------"); 
+        System.out.println("-----------------------");
         int i = Runtime.getRuntime().availableProcessors();
         System.out.println("i = " + i);
     }
+
     @Test
-    public void fun1(){
+    public void fun1() {
         long i = 100_000_000_000L;
         float v = 9.1f;
         float v1 = 9.000f;
-        System.out.println( v%1==0);
-        System.out.println( v1%1==0);
+        System.out.println(v % 1 == 0);
+        System.out.println(v1 % 1 == 0);
     }
+
     @Test
-    public void fun(){
-        int i = ((6-3) >> 1 )+3;
+    public void fun() {
+        int i = ((6 - 3) >> 1) + 3;
         System.out.println("i = " + i);
     }
-    
-    
+
+    //滑稽的大佬
+    @Test
+    public void fun2() {
+        System.out.println(129 == 129);
+    }
+
 }
