483b62e89fe83924b241a5105404f3f9cffb9e3f
yi-yun <yi--yun@outlook.com>
2019-06-10 12:22:38 +0800

    add bug

--- java_se/src/main/java/com/yiyun/java8/chap8/Debugging.java
+++ java_se/src/main/java/com/yiyun/java8/chap8/Debugging.java
@@ -5,26 +5,39 @@ import lombok.Getter;
 import lombok.Setter;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class Debugging {
     @Test
     public void testComparing() throws Exception{
-        
+//        Point point = new Point(10, 12);
+//        Point point1 = new Point(20, 31);
+    }
+
+    public static void main(String[] args) {
+        List<Point> points = Arrays.asList(new Point(12, 2), null);
+        points.stream().map(Point::getX).forEach(System.out::println);
     }
 }
 
 @Getter
 @AllArgsConstructor
 @Setter
-class Point {
+class Point implements Comparable{
     private final int x;
     private final int y;
 
     public Point moveRightBy(int x) {
         return new Point(this.x + x, y);
     }
-
+    
     public final static Comparator<Point> compareByXAndThenY =
         Comparator.comparing(Point::getX).thenComparing(Point::getY);
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
