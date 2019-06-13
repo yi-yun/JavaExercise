ec3969bacf224f948ac19353b5a1d92e69377ea3
yi-yun <yi--yun@outlook.com>
2019-06-13 16:48:25 +0800

    add java8 in action chap9&10

--- java_se/src/main/java/com/yiyun/java8/chap10/OptionalMain.java
+++ java_se/src/main/java/com/yiyun/java8/chap10/OptionalMain.java
@@ -1,5 +1,22 @@
 package com.yiyun.java8.chap10;
 
+import org.junit.Test;
+
+import java.util.Optional;
+
 public class OptionalMain {
-    
+    @Test
+    public void fun() {
+        Insurance insurance = new Insurance();
+        Optional<Insurance> optionalInsurance = Optional.ofNullable(insurance);
+        Optional<String> name = optionalInsurance.map(Insurance::getName);
+        System.out.println("name = " + name);
+    }
+
+    public String getCarInsuranceName(Optional<Person> person) {
+        return person.flatMap(Person::getCar)
+            .flatMap(Car::getInsurance)
+            .map(Insurance::getName)
+            .orElse("Unknown");
+    }
 }
