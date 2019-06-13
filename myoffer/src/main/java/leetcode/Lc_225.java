333e0326f549dea073b65da9c6c329af1be5c624
yi-yun <yi--yun@outlook.com>
2019-06-13 20:21:02 +0800

    add 2queueStack

---
+++ myoffer/src/main/java/leetcode/Lc_225.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Lc_225 {
+}
+class MyStack {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1=new LinkedList<>();
+        q2=new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.offer(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        while (q1.size()>1)
+            q2.offer(q1.poll());
+        Integer poll = q1.poll();
+        while (!q2.isEmpty())
+            q1.offer(q2.poll());
+        return poll;
+    }
+
+    /** Get the top element. */
+    public int top() {
+        while (q1.size()>1)
+            q2.offer(q1.poll());
+        Integer poll = q1.peek();
+        q2.offer(q1.poll());
+        while (!q2.isEmpty())
+            q1.offer(q2.poll());
+        return poll;
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
