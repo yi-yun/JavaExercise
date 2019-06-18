f7d532eba8ed782927e521a6a3dfbbea2572a4c6
yi-yun <yi--yun@outlook.com>
2019-06-18 23:03:56 +0800

    add LC_226

---
+++ myoffer/src/main/java/leetcode/LC_226.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC_226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+        return root;
+    }
+}
