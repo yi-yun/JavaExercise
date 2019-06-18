e38440b711962301b5d1a87e45af2213921efce5
yi-yun <yi--yun@outlook.com>
2019-06-18 22:41:31 +0800

    add HashMap Entry Test

--- java_test/src/main/java/com/yiyun/TesthashMap.java
+++ java_test/src/main/java/com/yiyun/TesthashMap.java
@@ -7,12 +7,24 @@ import java.util.Map;
 
 public class TesthashMap {
     @Test
-    public void fun(){
+    public void fun1() {
         Map<Integer, String> map = new HashMap<>();
-        map.put(1,"yiyun1");
-        map.put(2,"yiyun2");
-        map.put(3,"yiyun3");
-        map.put(4,"yiyun4");
+        map.put(1, "yiyun1");
+        map.put(2, "yiyun2");
+        map.put(3, "yiyun3");
+        map.put(4, "yiyun4");
         System.out.println("map = " + map);
+    }
+
+    @Test
+    public void fun2() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "yiyun1");
+        map.put(2, "yiyun2");
+        map.put(3, "yiyun3");
+        map.put(4, "yiyun4");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("键" + entry.getKey() + ":" + entry.getValue());
+        }
     }
 }
