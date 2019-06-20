d9fc19fba12fbb622eb0d4002a7b02bf4c5a8a8a
yi-yun <yi--yun@outlook.com>
2019-06-20 12:12:17 +0800

    add LC 1008

---
+++ myoffer/src/main/java/leetcode/LC_1008.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class LC_1008 {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 1; i < preorder.length; i++) {
+            helper(root, preorder[i]);
+        }
+        return root;
+    }
+
+    private void helper(TreeNode root, int val) {
+        if (root.val > val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+                return;
+            } else helper(root.left, val);
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+                return;
+            } else helper(root.right, val);
+        }
+    }
+}
