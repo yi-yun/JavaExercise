68d32ad611b8d39cf77d4b0d38b667e46ba32870
yi-yun <yi--yun@outlook.com>
2019-06-21 20:06:30 +0800

    add TestClass

---
+++ java_test/src/main/java/com/yiyun/TestClass.java
@@ -0,0 +1,26 @@
+package com.yiyun;
+
+import org.junit.Test;
+
+public class TestClass {
+    static int a;
+    void fun(){
+        a=10;
+    }
+    @Test
+    public void fun2(){
+        int x=1;
+        float y=2;
+        System.out.println(x/y);
+    }
+    @Test
+    public void fun3(){
+        
+    }
+    protected class A{
+        
+    }
+    private class B{}
+    class C{ }
+    public class D{}
+}
