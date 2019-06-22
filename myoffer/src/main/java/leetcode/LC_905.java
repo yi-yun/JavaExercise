8b4842f802aaca4e84dbf4c1b920988d92e02d92
yi-yun <yi--yun@outlook.com>
2019-06-22 16:57:05 +0800

    add LC_905

---
+++ myoffer/src/main/java/leetcode/LC_905.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class LC_905 {
+    public int[] sortArrayByParity(int[] A) {
+        int z=0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i]%2==0){
+                int temp=A[z];
+                A[z]=A[i];
+                A[i]=temp;
+                z++;
+            }
+        }
+        return A;
+    }
+}
