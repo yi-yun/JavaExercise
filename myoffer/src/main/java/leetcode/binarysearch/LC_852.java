23cc903ae8553bc45811c0f8ef18be57c8b30c15
yi-yun <yi--yun@outlook.com>
2019-06-24 22:42:45 +0800

    add left<right-1

--- myoffer/src/main/java/leetcode/binarysearch/LC_852.java
+++ myoffer/src/main/java/leetcode/binarysearch/LC_852.java
@@ -18,7 +18,22 @@ public class LC_852 {
         }
         return -1;
     }
-
+    
+    public int peakIndexInMountainArray2(int[] A){
+        int length = A.length;
+        int left=0,right=length-1;
+        while (left<right-1){
+            int mid = ((right - left) >> 1) + left;
+            if (A[mid]>A[mid-1]&&A[mid]>A[mid+1]){
+                return mid;
+            }else if (A[mid]<A[mid-1]&&A[mid]>A[mid+1]){
+                right=mid;
+            }else if (A[mid]>A[mid-1]&&A[mid]<A[mid+1]){
+                left=mid;
+            }
+        }
+        return A[left]>A[right]?left:right;
+    }
     @Test
     public void fun() {
         int[] arrays = {0, 1, 2, 3, 1, 0};
