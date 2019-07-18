0e8fa1c83c338bf8dd9fc888ae2a99171427f128
yi-yun <yi--yun@outlook.com>
2019-07-18 23:52:18 +0800

    mod backpack possible

--- myoffer/src/main/java/lintcode/_125_backpack2.java
+++ myoffer/src/main/java/lintcode/_125_backpack2.java
@@ -46,11 +46,23 @@ public class _125_backpack2 {
         // write your code here
         int n = A.length;
         int[] states = new int[m + 1];
-        for (int i = 0; i < m + 1; i++) {
-            states[i] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = m - A[i]; j >= 0; j--) {
+                states[j + A[i]] = Integer.max(states[j] + V[i], states[j + A[i]]);
+            }
         }
+        return states[m];
+    }
+
+    //尽量装满 m 最大重量  A 重量数组    V 价值的数组
+    public int backPackIITest(int m, int[] A, int[] V) {
+        // write your code here
+        int n = A.length;
+        int[] states = new int[m + 1];
         if (A[0] <= m) {
-            states[A[0]] = V[0];
+            for (int i = A[0]; i <= m; i++) {
+                states[i] = V[0];
+            }
         }
         for (int i = 1; i < n; i++) {
             for (int j = m - A[i]; j >= 0; j--) {
@@ -63,12 +75,12 @@ public class _125_backpack2 {
 
     @Test
     public void fun1() {
-        backPackII(10, new int[]{2, 3, 5, 7}, new int[]{1, 5, 2, 4});
+        backPackII2(10, new int[]{2, 3, 5, 7}, new int[]{1, 5, 2, 4});
     }
 
     @Test
     public void fun2() {
-        backPackII2(200,
+        backPackIITest(200,
             new int[]{79, 58, 86, 11, 28, 62, 15, 68},
             new int[]{83, 14, 54, 79, 72, 52, 48, 62});
     }
