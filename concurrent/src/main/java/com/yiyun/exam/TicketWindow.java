7fc65d85086804d20b3897d1a2dd62fbf02c9373
yi-yun <yi--yun@outlook.com>
2019-07-26 21:34:35 +0800

    add concurrent ticket

--- concurrent/src/main/java/com/yiyun/exam/TicketWindow.java
+++ concurrent/src/main/java/com/yiyun/exam/TicketWindow.java
@@ -1,5 +1,6 @@
 package com.yiyun.exam;
 
+import java.sql.Timestamp;
 import java.util.Random;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,7 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
  **/
 
 public class TicketWindow implements Runnable {
-    private int ticket = 20;
+    private int ticket = 100;
 
     ReentrantLock rt = new ReentrantLock(true);
 
@@ -22,14 +23,15 @@ public class TicketWindow implements Runnable {
             if (ticket <= 0) {
                 break;
             }
-            ticket--;
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.println(timestamp + "------------" + Thread.currentThread().getName() + "卖出第" + ticket + "张火车票");
             Random random = new Random();
             try {
-                Thread.sleep(1000 * random.nextInt(3));
+                Thread.sleep(1000 * random.nextInt(1));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Thread.currentThread().getName() + "卖出一张火车票,还剩" + ticket + "张");
+            ticket--;
             rt.unlock();
         }
     }
@@ -39,9 +41,13 @@ public class TicketWindow implements Runnable {
         Thread t1 = new Thread(tw, "一号窗口");
         Thread t2 = new Thread(tw, "二号窗口");
         Thread t3 = new Thread(tw, "三号窗口");
+        Thread t4 = new Thread(tw, "四号窗口");
+        Thread t5 = new Thread(tw, "五号窗口");
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
+        t5.start();
     }
 
 }
