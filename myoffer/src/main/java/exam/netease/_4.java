296e2a59526953badbd2d680337f84203fd3b2b6
yi-yun <yi--yun@outlook.com>
2019-08-03 21:01:28 +0800

    adjust

--- myoffer/src/main/java/exam/netease/_4.java
+++ myoffer/src/main/java/exam/netease/_4.java
@@ -2,90 +2,17 @@ package exam.netease;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @class: _3
- * @description: TODO
+ * @description: 序列维护，大于的数-1 并统计个数
  * @author: yi-yun
  * @create: 2019-08-03 15:37
  **/
 public class _4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int q = sc.nextInt();
-        //value  cnt
-        int max = Integer.MIN_VALUE;
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int nextInt = sc.nextInt();
-            max = Integer.max(nextInt, max);
-            if (hashMap.containsKey(nextInt)) {
-                Integer cnt = hashMap.get(nextInt);
-                hashMap.put(nextInt, cnt + 1);
-            } else {
-                hashMap.put(nextInt, 1);
-            }
-        }
-        for (int i = 0; i < q; i++) {
-            int arg = sc.nextInt(), cnt = 0;
-            for (int j = arg; j <= max; j++) {
-                if (hashMap.containsKey(j)) {
-                    Integer value = hashMap.get(j);
-                    cnt += value;
-                    int orgin = j - 1;
-                    if (hashMap.containsKey(orgin)) {
-                        hashMap.put(orgin, hashMap.get(orgin) + value);
-                    } else {
-                        hashMap.put(orgin, value);
-                    }
-                }
-            }
-            //System.out.println(Arrays.toString(array));
-            System.out.println(cnt);
-        }
-    }
 
-    private static int cal(int arg, Map<Integer, Integer> hashMap, int max) {
-        int cnt = 0;
-        for (int j = arg; j <= max; j++) {
-            if (hashMap.containsKey(j)) {
-                Integer value = hashMap.get(j);
-                cnt += value;
-                int orgin = j - 1;
-                if (hashMap.containsKey(orgin)) {
-                    hashMap.put(orgin, hashMap.get(orgin) + value);
-                } else {
-                    hashMap.put(orgin, value);
-                }
-            }
-        }
-        return cnt;
-    }
-
-    @Test
-    public void fun() {
-        int[] ints = {1, 2, 3, 4, 5};
-        for (int j = 0; j < 3; j++) {
-            for (int i = 0; i < ints.length; i++) {
-                ints[i]--;
-            }
-        }
-
-    }
-}
-/*import java.util.*;
-
-*
- * @class: _3
- * @description: TODO
- * @author: yi-yun
- * @create: 2019-08-03 15:37
- *
-public class Main {
+    //暴力较慢
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -107,4 +34,53 @@ public class Main {
             System.out.println(cnt);
         }
     }
-}*/
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        Arrays.sort(array);
+        for (int i = 0; i < q; i++) {
+            int arg = sc.nextInt(), cnt = 0;
+            //既然排序了用二分法出来位置然后前面的数-1
+            //排除最大最小，防止下标越界
+        }
+    }
+
+    private static int find(int[] a, int key) {
+        int left = 0, right = a.length - 1;
+        //left,num,right
+        while (left < right - 1) {
+            int mid = (right - left) / 2 + left;
+            if (a[mid] > key) {
+                right = mid;
+            } else if (a[mid] < key) {
+                left = mid;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+
+    @Test
+    public void fun() {
+        int[] ints = {1, 2, 3, 4, 5};
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < ints.length; i++) {
+                ints[i]--;
+            }
+        }
+
+    }
+
+    @Test
+    public void fun1() {
+        int[] ints = {1, 4, 5, 7, 9};
+        find(ints, 6);
+    }
+}
