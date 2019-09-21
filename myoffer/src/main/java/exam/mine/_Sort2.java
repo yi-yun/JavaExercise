3c1f44143b59de03765ad49a83a74864677ca4f4
yi-yun <yi--yun@outlook.com>
2019-09-21 19:03:18 +0800

    add sort practice

--- myoffer/src/main/java/exam/mine/_Sort2.java
+++ myoffer/src/main/java/exam/mine/_Sort2.java
@@ -11,6 +11,46 @@ import java.util.Arrays;
  * @create: 2019-09-07 21:11
  **/
 public class _Sort2 {
+    @Test
+    public void funMerge() {
+        int[] a = {1, 8, 5, 3, 7, 4, 2, 3};
+        mergeSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+
+
+    public void mergeSort(int[] a, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (right - left) / 2 + left;
+        mergeSort(a, left, mid);
+        mergeSort(a, mid + 1, right);
+        merge(a, left, mid, right);
+    }
+
+    private void merge(int[] a, int left, int mid, int right) {
+        int[] helper = new int[right - left + 1];
+        int i = left, j = mid + 1, index = 0;
+        while (i <= mid && j <= right) {
+            if (a[i] <= a[j]) {
+                helper[index++] = a[i++];
+            } else {
+                helper[index++] = a[j++];
+            }
+        }
+        while (i <= mid) {
+            helper[index++] = a[i++];
+        }
+        while (j <= right) {
+            helper[index++] = a[j++];
+        }
+        for (int k = 0; k < helper.length; k++) {
+            a[k + left] = helper[k];
+        }
+    }
+
+
     public void bubbleSort(int[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - 1 - i; j++) {
@@ -94,7 +134,7 @@ public class _Sort2 {
             }
         }
     }
-    
+
     @Test
     public void fun2() {
         int[] a = {1, 3, 2, 4, 8, 2};
